54bf35fc96058a0dbfce435dcba624cc34bb8e84
Pavel Nadolski <tbigad@gmail.com>
2020-04-06 03:00:48 +0300

    complite task2

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,29 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    NSInteger cards = [array[0] integerValue];
+    NSInteger desks = [array[1] integerValue];
+    
+    for (long i = 0; i < desks; i++) {
+        if ([self binnominalCoefficent:desks with:i] == cards)
+            return [NSNumber numberWithInteger:i];
+    }
+    
+    return nil;
 }
+
+- (long)binnominalCoefficent:(long)n with:(long)k {
+    long r = 1;
+    long d;
+    if (k > n) return 0;
+    for (d = 1; d <= k; d++)
+    {
+       r *= n--;
+       r /= d;
+    }
+    return r;
+}
+
+
+
 @end
